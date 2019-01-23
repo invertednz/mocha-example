1383504816017f014242b96cad322ad46810a515
invertednz <jamesfarrier@gmail.com>
2019-01-23 18:39:09 +1300

    Update the test names

--- test/test2.js
+++ test/test2.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present2', function() {
+    it('should return -1 when the value is not present 2', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });
