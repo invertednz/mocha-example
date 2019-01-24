5fe0064a84eff4683858834e357d49a393039bca
invertednz <jamesfarrier@gmail.com>
2019-01-25 11:43:49 +1300

    Add additional test

--- test/test.js
+++ test/test.js
@@ -13,3 +13,10 @@ describe('Array', function() {
     });
   });
 });
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('starts should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
